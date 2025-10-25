836f5b7c1e0319fef644b3e865a9013b7e29dddf
fatimaalhassan <um.wasan33@gmail.com>
2025-10-25 13:20:45 +0300

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+import { useState } from 'react';
+import PokemonList from './components/PokemonList/PokemonList';
+
+import { Route, Routes } from 'react-router';
+
+import NavBar from './components/NavBar/NavBar';
+
+import { Link } from 'react-router';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
+import { useParams } from 'react-router';
+
+import PokemonForm from './components/PokemonForm/PokemonForm';
+
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+];
+
+const App = () => {
+  const [pokemon, setPokemon] = useState(initialState);
+
+
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1>Pokemon!</h1>
+      <NavBar />
+  
 
-export default App
+    
+     <Routes>
+        <Route path='/' element={<h2>Home Page</h2>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+        {/* Updated route passing PokemonForm the addPokemon prop */}
+        <Route 
+          path="/pokemon/new" 
+          element={<PokemonForm addPokemon={addPokemon} />}
+        />
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
+
+    </>
+  );}
+
+
+
+
+export default App;
---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,22 @@
+// src/components/PokemonList/PokemonList.jsx
+
+const PokemonList = (props) => {
+  return (
+    <>
+      <h2>Pokemon</h2>
+      <ul>
+        {props.pokemon.map((currentPokemon) => (
+
+          <li key={currentPokemon.name}>
+            <Link to={`/pokemon/${currentPokemon._id}`}>
+              {currentPokemon.name}
+            </Link>
+          </li>
+
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PokemonList;
